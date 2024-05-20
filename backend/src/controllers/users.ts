a41b9464bde83f77c6da51b4673e01e8fe931ddf
Poxie <poxen12@hotmail.com>
2024-05-20 23:00:04 +0200

    Basic prisma user fetch

---
+++ backend/src/controllers/users.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+const router = express.Router();
+
+router.get('/', async (req: Request, res: Response) => {
+    const users = await prisma.user.findMany();
+    res.send(users);
+});
+
+export default router;
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import users from '../controllers/users';
 import recipes from '../controllers/recipes';
 
 const router = Router();
 
+router.use('/users', users);
 router.use('/recipes', recipes);
 
 export default router;
