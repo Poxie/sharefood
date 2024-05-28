f6ca813ea7029fb5dc86e28dc85896464a6e43c2
Poxie <poxen12@hotmail.com>
2024-05-28 18:46:31 +0200

    Add route & utils for creating a user

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
+import Users from '../utils/users';
+import { UsernameAlreadyTakenError } from '@/errors/UsernameAlreadyTakenError';
 
 const prisma = new PrismaClient();
 const router = express.Router();
@@ -7,6 +9,23 @@ const router = express.Router();
 router.get('/', async (req: Request, res: Response) => {
     const users = await prisma.user.findMany();
     res.send(users);
+});
+
+router.post('/', async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await Users.createUser({ 
+            username, 
+            password, 
+        })
+        return res.send(user);
+    } catch(error) {
+        // If the username is already taken, return a 401 status code
+        if(error instanceof UsernameAlreadyTakenError) {
+            return res.status(error.statusCode).send(error.message);
+        }
+    }
 });
 
 export default router;
--- backend/src/utils/users.ts
+++ backend/src/utils/users.ts
@@ -1,6 +1,8 @@
+import bcrypt from 'bcrypt';
 import { PrismaClient, User } from "@prisma/client";
 import { ID_LENGTH } from "./constants";
 import prisma from "@/../client";
+import { UsernameAlreadyTakenError } from '@/errors/UsernameAlreadyTakenError';
 
 export const generateUserId: () => Promise<string> = async () => {
     const id = Math.random().toString(36).slice(2, ID_LENGTH + 2);
@@ -11,13 +13,43 @@ export const generateUserId: () => Promise<string> = async () => {
     return id;
 }
 
+// Exclude keys from user
+function exclude<User, Key extends keyof User>(
+    user: User,
+    keys: Key[]
+): Omit<User, Key> {
+    return Object.fromEntries(
+        Object.entries(user as { [k: string]: unknown; }).filter(([key]) => !keys.includes(key as Key))
+    ) as Omit<User, Key>;
+}
 export default class Users {
     static async getUserById(id: string) {
         const user = await prisma.user.findUnique({ where: { id } });
         if(!user) return null;
 
-        return Object.fromEntries(
-            Object.entries(user).filter(([key]) => key !== 'password')
-        ) as Omit<User, 'password'>
+        return exclude(user, ['password']);
+    }
+    static async createUser({ username, password }: { 
+        username: string, password: string 
+    }) {
+        const hashedPassword = await bcrypt.hash(password, parseInt(process.env.BCRYPT_SALT_ROUNDS || '10'));
+
+        try {
+            const user = await prisma.user.create({
+                data: {
+                    id: await generateUserId(),
+                    username,
+                    password: hashedPassword,
+                    createdAt: new Date().getTime().toString(),
+                }
+            });
+            
+            return exclude(user, ['password']);
+        } catch(error) {
+            if((error as any).code === 'P2002') {
+                throw new UsernameAlreadyTakenError();
+            }
+            throw error;
+        }
     }
 }
