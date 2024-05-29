d54b6e2053046dc857d480a266a728b2a0f8a173
Poxie <poxen12@hotmail.com>
2024-05-29 12:54:17 +0200

    Check validity of jwt private key

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -3,5 +3,6 @@ import jwt from 'jsonwebtoken'
 const JWT_PRIVATE_KEY = process.env.JWT_PRIVATE_KEY;
 
 export const signToken = (userId: string) => {
+    if(!JWT_PRIVATE_KEY) throw new Error('JWT_PRIVATE_KEY is not defined');
     return jwt.sign({ userId }, JWT_PRIVATE_KEY);
 }
