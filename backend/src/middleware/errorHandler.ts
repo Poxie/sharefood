9c80f9044da3b057b5aee04eb483560c9d6347c0
Poxie <poxen12@hotmail.com>
2024-05-30 22:52:31 +0200

    Send error messages in objects

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -4,10 +4,10 @@ import { NextFunction, Request, Response } from "express";
 
 export default function errorHandler(error: Error | CustomError, req: Request, res: Response, next: NextFunction) {
     if(error instanceof CustomError) {
-        return res.status(error.statusCode).send(error.message);
+        return res.status(error.statusCode).send({ message: error.message });
     }
   
     // If it's not a known error, send a generic 500 Internal Server Error
     console.error(error);
-    return res.status(ERROR_CODES.INTERNAL_SERVER_ERROR).send('Internal Server Error');
+    return res.status(ERROR_CODES.INTERNAL_SERVER_ERROR).send({ message: 'Internal Server Error' });
 }
