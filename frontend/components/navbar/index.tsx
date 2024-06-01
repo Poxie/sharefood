70a37e78934e89786c91c962a5adc178ee9c58c7
Poxie <poxen12@hotmail.com>
2024-06-01 22:49:57 +0200

    Decrease navbar padding

--- frontend/components/navbar/index.tsx
+++ frontend/components/navbar/index.tsx
@@ -7,14 +7,14 @@ import NavbarOptions from "./NavbarOptions";
 export default function Navbar() {
     return(
         <div className="border-b-[1px] border-b-tertiary">
-            <nav className="w-main max-w-main mx-auto py-8 flex items-center justify-between">
+            <nav className="w-main max-w-main mx-auto py-6 flex items-center justify-between">
                 <div className="flex items-center gap-6">
                     <Link   
                         data-testid="navbar-logo" 
                         href={"/"}
                     >
                         <Image 
-                            width={128}
+                            width={148}
                             height={14}
                             src="/logo.svg"
                             alt="Logo"
