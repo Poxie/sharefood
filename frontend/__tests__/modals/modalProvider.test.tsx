35796f7341ae604fc0fd9131c756ff6b8dc5a946
Poxie <poxen12@hotmail.com>
2024-06-02 16:00:25 +0200

    Due to new provider in test-utils, use regular render method to check for used outside modal provider error

--- frontend/__tests__/modals/modalProvider.test.tsx
+++ frontend/__tests__/modals/modalProvider.test.tsx
@@ -1,4 +1,5 @@
 import '@testing-library/jest-dom';
+import { render as renderWithoutProviders } from '@testing-library/react';
 import { fireEvent, render, screen } from '@/test-utils';
 import ModalProvider, { useModal } from '../../contexts/modal';
 import Modal from '@/modals';
@@ -16,36 +17,24 @@ const CustomTest = () => {
 
 describe('ModalProvider', () => {
     it('should render the children', () => {
-        render(
-            <ModalProvider>
-                <span data-testid="regular-body-content" />
-            </ModalProvider>
-        )
+        render(<span data-testid="regular-body-content" />)
 
         const content = screen.getByTestId('regular-body-content');
         expect(content).toBeInTheDocument();
     })
     it('should render the modal container', () => {
-        render(
-            <ModalProvider>
-                <span data-testid="regular-body-content" />
-            </ModalProvider>
-        )
+        render(<span data-testid="regular-body-content" />)
 
         const container = screen.getByTestId('modal-container');
         expect(container).toBeInTheDocument();
     })
     it('should throw an error if used outside of the provider', () => {
         jest.spyOn(console, 'error').mockImplementation(() => jest.fn());
-        expect(() => render(<CustomTest />)).toThrow('useModal must be used within a ModalProvider');
+        expect(() => renderWithoutProviders(<CustomTest />)).toThrow('useModal must be used within a ModalProvider');
         jest.restoreAllMocks();
     })
     it('should render the modal component when set using hook', () => {
-        render(
-            <ModalProvider>
-                <CustomTest />
-            </ModalProvider>
-        )
+        render(<CustomTest />)
 
         const openButton = screen.getByText('Open Modal');
         fireEvent.click(openButton);
@@ -54,11 +43,7 @@ describe('ModalProvider', () => {
         expect(modal).toBeInTheDocument();
     })
     it('should close the modal when closeModal function is called', () => {
-        render(
-            <ModalProvider>
-                <CustomTest />
-            </ModalProvider>
-        )
+        render(<CustomTest />)
 
         const openButton = screen.getByText('Open Modal');
         fireEvent.click(openButton);
