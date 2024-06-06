e8143aee40425f9d4f7a4587844c47e0d13356a0
Poxie <poxen12@hotmail.com>
2024-06-06 19:36:12 +0200

    Rewrite tests for createUser function

--- frontend/__tests__/api/user/createUser.test.ts
+++ frontend/__tests__/api/user/createUser.test.ts
@@ -1,27 +1,39 @@
-import * as fetchAPI from "@/api";
-import { createUser } from "@/api/user";
+import nock from 'nock';
 import mockUser from "@/test-constants";
-
-jest.mock('@/api');
+import { createUser } from '@/api/user';
 
 describe('createUser', () => {
-    it('should create a user', async () => {
-        const password = 'password';
+    const user = mockUser();
+    const password = 'password';
+
+    const requestBody = { username: user.username, password };
+    const nockRequest = (reply: {
+        status: number,
+        body: any,
+    }) => {
+        nock(process.env.NEXT_PUBLIC_API_URL)
+            .post('/users', requestBody)
+            .reply(reply.status, reply.body);
+    }
+
+    it('should return the user data & access token when request is successful', async () => {
+        const user = mockUser();
         const data = {
             user: mockUser(),
             accessToken: 'accesstoken',
         }
-        const spy = jest.spyOn(fetchAPI, 'default').mockResolvedValue(data);
+        
+        nockRequest({ status: 200, body: data });
 
-        const response = await createUser({
-            username: data.user.username,
-            password,
-        });
+        const response = await createUser(requestBody);
 
         expect(response).toEqual(data);
-        expect(spy).toHaveBeenCalledWith('/users', {
-            method: 'POST',
-            body: JSON.stringify({ username: data.user.username, password }),
-        })
+    })
+    it('should throw an error if the request is not successful', async () => {
+        const errorMessage = 'An error occurred';
+        
+        nockRequest({ status: 400, body: { message: errorMessage } });
+
+        await expect(createUser(requestBody)).rejects.toThrow(new Error(errorMessage));
     })
 })
