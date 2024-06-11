af5a927aaa306802ad9a586736860289de602412
Poxie <poxen12@hotmail.com>
2024-06-11 21:29:57 +0200

    Add helpder functions to button tests

--- frontend/__tests__/button.test.tsx
+++ frontend/__tests__/button.test.tsx
@@ -1,62 +1,70 @@
 import Button from '@/components/button'
 import '@testing-library/jest-dom'
-import { render, screen } from '@/test-utils'
+import { fireEvent, render, screen } from '@/test-utils'
  
 describe('Button', () => {
+    const renderButton = (props?: React.ComponentProps<typeof Button>) => {
+        render(<Button {...props}>{props?.children || 'Button text'}</Button>)
+    }
+
+    const getButton = () => screen.getByRole('button');
+
     it('renders a button with the correct text', () => {
-        const text = 'Button text';
-        render(<Button>{text}</Button>)
+        const text = 'Specific button text';
+        renderButton({ children: text });
     
-        const button = screen.getByRole('button');
+        const button = getButton();
     
         expect(button).toBeInTheDocument()
         expect(button).toHaveTextContent(text);
     })
     it('calls the onClick function when clicked', () => {
         const onClick = jest.fn();
-        render(<Button onClick={onClick}>Button text</Button>)
+
+        renderButton({ onClick });
     
-        const button = screen.getByRole('button');
-        button.click();
+        const button = getButton();
+        fireEvent.click(button);
     
         expect(onClick).toHaveBeenCalledTimes(1);
     })
     it('should have the primary button classes by default', () => {
-        render(<Button>Button text</Button>)
+        renderButton();
     
-        const button = screen.getByRole('button');
+        const button = getButton();
     
         expect(button).toHaveClass('text-light');
         expect(button).toHaveClass('bg-c-primary');
         expect(button).toHaveClass('hover:bg-c-accent');
     })
     it('should have the transparent button classes when type is transparent', () => {
-        render(<Button type="transparent">Button text</Button>)
+        renderButton({ type: 'transparent' });
     
-        const button = screen.getByRole('button');
+        const button = getButton()
     
         expect(button).toHaveClass('hover:bg-secondary');
     })
     it('should have the hollow button classes when type is hollow', () => {
-        render(<Button type="hollow">Button text</Button>)
+        renderButton({ type: 'hollow' });
     
-        const button = screen.getByRole('button');
+        const button = getButton();
     
         expect(button).toHaveClass('border-[1px]');
         expect(button).toHaveClass('border-text-muted');
         expect(button).toHaveClass('hover:bg-tertiary');
     })
     it('should be an anchor tag if href is provided', () => {
-        render(<Button href="https://example.com">Button text</Button>)
+        const href = 'https://example.com';
+        renderButton({ href });
     
         const button = screen.getByRole('link');
     
-        expect(button).toHaveAttribute('href', 'https://example.com');
+        expect(button).toHaveAttribute('href', href);
     })
     it('should be disabled if disabled is true', () => {
-        render(<Button disabled>Button text</Button>)
+        renderButton({ disabled: true });
     
-        const button = screen.getByRole('button');
+        const button = getButton();
     
         expect(button).toBeDisabled();
     })
