f2111a8bec13a97e5631b9a7496c69efdc545fa8
Poxie <poxen12@hotmail.com>
2024-06-12 01:24:25 +0200

    Tests for delete user mutation

--- backend/src/__tests__/utils/users/userMutations.test.ts
+++ backend/src/__tests__/utils/users/userMutations.test.ts
@@ -5,6 +5,7 @@ import { prismaMock } from "../../../../singleton";
 import { PRISMA_ERROR_CODES } from "@/errors/errorCodes";
 import { UsernameAlreadyTakenError } from "@/errors/UsernameAlreadyTakenError";
 import UserUtils from "@/utils/users/userUtils";
+import UserNotFoundError from "@/errors/UserNotFoundError";
 
 describe('userMutations', () => {
     afterEach(() => {
@@ -45,6 +46,27 @@ describe('userMutations', () => {
             const prismaSpy = prismaMock.user.create.mockRejectedValue({ code: PRISMA_ERROR_CODES.UNIQUE_CONSTRAINT_VIOLATION });
 
             await expect(UserMutations.createUser({ username, password })).rejects.toThrow(new UsernameAlreadyTakenError());
+        })
+    })
+
+    describe('deleteUser', () => {
+        it('deletes a user and returns true', async () => {
+            const user = mockUser();
+            const userId = user.id;
+
+            const prismaSpy = prismaMock.user.delete.mockResolvedValue(user);
+
+            const deleted = await UserMutations.deleteUser(userId);
+
+            expect(deleted).toBe(true);
+            expect(prismaSpy).toHaveBeenCalledWith({ where: { id: userId } });
+        })
+        it('throws an error if the user is not found', async () => {
+            const userId = 'invalidid';
+
+            const prismaSpy = prismaMock.user.delete.mockRejectedValue({ code: PRISMA_ERROR_CODES.RECORD_NOT_FOUND });
+
+            await expect(UserMutations.deleteUser(userId)).rejects.toThrow(new UserNotFoundError());
         })
     })
 })
