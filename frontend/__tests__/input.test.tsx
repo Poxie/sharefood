7a5ec82597be697ba19cfd334d7629d398a6158a
Poxie <poxen12@hotmail.com>
2024-06-11 21:25:49 +0200

    Group tests & use helper functions to reduce duplication

--- frontend/__tests__/input.test.tsx
+++ frontend/__tests__/input.test.tsx
@@ -1,99 +1,112 @@
 import '@testing-library/jest-dom'
 import SendIcon from '@/assets/icons/SendIcon';
 import Input from '@/components/input'
-import { fireEvent, render, screen } from '@/test-utils';
+import { fireEvent, render, screen, updateInput } from '@/test-utils';
 
 describe('Input', () => {
-    it('should render an input element', () => {
-        const { getByRole } =  render(<Input />);
-        const input = getByRole('textbox');
-        expect(input).toBeInTheDocument();
+    const renderInput = (props?: React.ComponentProps<typeof Input>) => {
+        render(<Input {...props} />);
+    }
+    const getInput = () => screen.getByRole('textbox');
+
+    describe('Rendering', () => {
+        it('should render an input element', () => {
+            renderInput();
+
+            const input = getInput();
+
+            expect(input).toBeInTheDocument();
+        })
+        it('should render the input with the correct placeholder', () => {
+            const placeholder = 'Search';
+    
+            renderInput({ placeholder });
+    
+            const input = screen.getByPlaceholderText(placeholder);
+    
+            expect(input).toBeInTheDocument();
+        })
+        it('should render a password input if type is password', () => {
+            const type = 'password';
+            const placeholder = 'Password';
+    
+            renderInput({ type, placeholder });
+    
+            const input = screen.getByPlaceholderText(placeholder);
+    
+            expect(input).toHaveAttribute('type', type);
+        });
+        it('should render the input with the default value', () => {
+            const value = 'Hello';
+    
+            renderInput({ defaultValue: value });
+    
+            const input = screen.getByDisplayValue(value);
+    
+            expect(input).toBeInTheDocument();
+        })
+        it('should render the icon element if provided', () => {
+            renderInput({ icon: <SendIcon /> });
+    
+            const icon = screen.getByTestId('input-icon');
+    
+            expect(icon).toBeInTheDocument();
+        })
     })
-    it('should render the input with the correct placeholder', () => {
-        const placeholder = 'Search';
-
-        const { getByPlaceholderText } = render(<Input placeholder={placeholder} />);
-
-        const input = getByPlaceholderText(placeholder);
-
-        expect(input).toBeInTheDocument();
+    describe('Function interaction', () => {
+        it('should call the onChange function when the value changes', () => {
+            const onChange = jest.fn();
+    
+            renderInput({ onChange });
+    
+            const input = getInput();
+    
+            const newValue = 'Hello';
+            updateInput(input, newValue);
+    
+            expect(onChange).toHaveBeenCalledWith(newValue);
+        })
+        it('should call the onSubmit function when the enter key is pressed', () => {
+            const onSubmit = jest.fn();
+    
+            const text = 'Hello';
+            renderInput({ onSubmit, defaultValue: text });
+    
+            const input = getInput();
+    
+            fireEvent.keyDown(input, { key: 'Enter' });
+    
+            expect(onSubmit).toHaveBeenCalledWith(text);
+        })
     })
-    it('should render a password input if type is password', () => {
-        const type = 'password';
-        const placeholder = 'Password';
-
-        const { getByPlaceholderText } = render(<Input type={type} placeholder={placeholder} />);
-
-        const input = getByPlaceholderText(placeholder);
-
-        expect(input).toHaveAttribute('type', type);
-    });
-    it('should render the input with the default value', () => {
-        const value = 'Hello';
-
-        const { getByDisplayValue } = render(<Input defaultValue={value} />);
-
-        const input = getByDisplayValue(value);
-
-        expect(input).toBeInTheDocument();
-    })
-    it('should render the icon element if provided', () => {
-        const { getByTestId } = render(<Input icon={<SendIcon />} />);
-
-        const icon = getByTestId('input-icon');
-
-        expect(icon).toBeInTheDocument();
-    })
-    it('should call the onChange function when the value changes', () => {
-        const onChange = jest.fn();
-
-        const { getByRole } = render(<Input onChange={onChange} />);
-
-        const input = getByRole('textbox');
-
-        const newValue = 'Hello';
-        fireEvent.change(input, { target: { value: newValue } });
-
-        expect(onChange).toHaveBeenCalledWith(newValue);
-    })
-    it('should call the onSubmit function when the enter key is pressed', () => {
-        const onSubmit = jest.fn();
-
-        const text = 'Hello';
-        const { getByRole } = render(<Input onSubmit={onSubmit} defaultValue={text} />);
-
-        const input = getByRole('textbox');
-
-        fireEvent.keyDown(input, { key: 'Enter' });
-
-        expect(onSubmit).toHaveBeenCalledWith(text);
-    })
-    it('should set className on the input element if provided', () => {
-        const className = 'text-primary';
-
-        const { getByRole } = render(<Input className={className} />);
-
-        const input = getByRole('textbox');
-
-        expect(input).toHaveClass(className);
-    })
-    it('should set containerClassName on the container element if provided', () => {
-        const className = 'text-primary';
-
-        const { getByTestId } = render(<Input containerClassName={className} />);
-
-        const container = getByTestId('input-container');
-
-        expect(container).toHaveClass(className);
-    })
-    it('should set iconClassName on the icon element if provided', () => {
-        const className = 'text-primary';
-
-        const { getByTestId } = render(<Input icon={<SendIcon />} iconClassName={className} />);
-
-        const icon = getByTestId('input-icon');
-
-        expect(icon).toHaveClass(className);
+    describe('set classNames correctly', () => {
+        it('should set className on the input element if provided', () => {
+            const className = 'text-primary';
+    
+            renderInput({ className });
+    
+            const input = getInput();
+    
+            expect(input).toHaveClass(className);
+        })
+        it('should set containerClassName on the container element if provided', () => {
+            const className = 'text-primary';
+    
+            renderInput({ containerClassName: className });
+    
+            const container = screen.getByTestId('input-container');
+    
+            expect(container).toHaveClass(className);
+        })
+        it('should set iconClassName on the icon element if provided', () => {
+            const className = 'text-primary';
+    
+            renderInput({ icon: <SendIcon />, iconClassName: className });
+    
+            const icon = screen.getByTestId('input-icon');
+    
+            expect(icon).toHaveClass(className);
+        })
     })
     describe('when the button icon is provided', () => {
         const renderInputWithButton = (props?: React.ComponentProps<typeof Input>) => {
@@ -105,10 +118,12 @@ describe('Input', () => {
             )
         }
 
+        const getIconButton = () => screen.getByRole('button');
+
         it('should render the button icon element if provided', () => {
-            renderInputWithButton()
+            renderInputWithButton();
     
-            const icon = screen.getByTestId('input-submit-icon');
+            const icon = getIconButton();
     
             expect(icon).toBeInTheDocument();
         })
@@ -121,7 +136,7 @@ describe('Input', () => {
                 defaultValue: text,
             })
 
-            const icon = screen.getByTestId('input-submit-icon');
+            const icon = getIconButton();
 
             fireEvent.click(icon);
 
@@ -132,7 +147,7 @@ describe('Input', () => {
 
             renderInputWithButton({ onButtonClick });
 
-            const icon = screen.getByTestId('input-submit-icon');
+            const icon = getIconButton();
 
             fireEvent.click(icon);
 
@@ -143,7 +158,7 @@ describe('Input', () => {
     
             renderInputWithButton({ buttonIconClassName: className });
     
-            const icon = screen.getByRole('button');
+            const icon = getIconButton();
     
             expect(icon).toHaveClass(className);
         })
