734596fd098db10eb79e13da7b09d3c8f0e2c91e
Poxie <poxen12@hotmail.com>
2024-06-12 01:48:47 +0200

    Tests for generateUserId function

--- backend/src/__tests__/utils/users/userUtils.test.ts
+++ backend/src/__tests__/utils/users/userUtils.test.ts
@@ -1,5 +1,8 @@
 import UserUtils from "@/utils/users/userUtils";
 import { mockUser } from "../../../../test-utils";
+import { USER_ID_LENGTH } from "@/utils/users/userConstants";
+import { prismaMock } from "../../../../singleton";
+import UserQueries from "@/utils/users/userQueries";
 
 describe('userUtils', () => {
     describe('excludeProperties', () => {
@@ -25,6 +28,32 @@ describe('userUtils', () => {
                     expect(result).not.toHaveProperty(property);
                 })
             })
+        })
+    })
+
+    describe('generateUserId', () => {
+        it(`returns a random string of ${USER_ID_LENGTH} characters`, async () => {
+            const id = await UserUtils.generateUserId();
+
+            expect(typeof id).toBe('string');
+            expect(id).toHaveLength(USER_ID_LENGTH);
+        })
+        it('returns a different id each time it is called', async () => {
+            const id1 = await UserUtils.generateUserId();
+            const id2 = await UserUtils.generateUserId();
+
+            expect(id1).not.toEqual(id2);
+        })
+        it('reruns the function if the generated id already exists', async () => {
+            const spy = jest.spyOn(UserQueries, 'getUserById').mockResolvedValueOnce(mockUser());
+
+            const id = await UserUtils.generateUserId();
+
+            expect(typeof id).toBe('string');
+            expect(id).toHaveLength(USER_ID_LENGTH);
+            
+            // To have been called twice, the first time the id already exists, the second time it doesn't
+            expect(spy).toHaveBeenCalledTimes(2);
         })
     })
 })
