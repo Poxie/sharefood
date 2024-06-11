10503b3317a1461f1d7d4e21b5803c99205ad973
Poxie <poxen12@hotmail.com>
2024-06-11 13:39:36 +0200

    Group modalprovider tests

--- frontend/__tests__/modals/modalProvider.test.tsx
+++ frontend/__tests__/modals/modalProvider.test.tsx
@@ -16,84 +16,91 @@ const CustomTest = () => {
 }
 
 describe('ModalProvider', () => {
-    it('should render the children', () => {
-        render(<span data-testid="regular-body-content" />)
+    const openModal = () => {
+        const openModalButton = screen.getByText('Open Modal');
+        fireEvent.click(openModalButton);
+    }
+    const closeModal = () => {
+        const closeModalButton = screen.getByText('Close Modal');
+        fireEvent.click(closeModalButton);
+    }
 
-        const content = screen.getByTestId('regular-body-content');
-        expect(content).toBeInTheDocument();
+    describe('Render the providers children and modal container', () => {
+        beforeEach(() => {
+            render(<span data-testid="regular-body-content" />);
+        })
+
+        it('should render the children', () => {
+            const content = screen.getByTestId('regular-body-content');
+            expect(content).toBeInTheDocument();
+        })
+        it('should render the modal container', () => {
+            const container = screen.getByTestId('modal-container');
+            expect(container).toBeInTheDocument();
+        })
     })
-    it('should render the modal container', () => {
-        render(<span data-testid="regular-body-content" />)
+    describe('useModal hook functions', () => {
+        beforeEach(() => {
+            render(<CustomTest />)
+        })
 
-        const container = screen.getByTestId('modal-container');
-        expect(container).toBeInTheDocument();
+        it('should render the modal component when set using hook', () => {
+            openModal();
+    
+            const modal = screen.getByTestId('modal');
+            expect(modal).toBeInTheDocument();
+        })
+        it('should close the modal when closeModal function is called', () => {
+            openModal();
+    
+            const closeButton = screen.getByText('Close Modal');
+            fireEvent.click(closeButton);
+    
+            const modal = screen.queryByTestId('modal');
+            expect(modal).not.toBeInTheDocument();
+        })
     })
-    it('should throw an error if used outside of the provider', () => {
-        jest.spyOn(console, 'error').mockImplementation(() => jest.fn());
-        expect(() => renderWithoutProviders(<CustomTest />)).toThrow('useModal must be used within a ModalProvider');
-        jest.restoreAllMocks();
+    describe('ModalProvider active state changes', () => {
+        beforeEach(() => {
+            render(<CustomTest />)
+        })
+
+        it('renders a backdrop when a modal is set', () => {
+            openModal();
+    
+            const backdrop = screen.getByTestId('modal-backdrop');
+            expect(backdrop).toBeInTheDocument();
+        })
+        it('closes the modal when the backdrop is clicked', () => {
+            openModal();
+    
+            const backdrop = screen.getByTestId('modal-backdrop');
+            fireEvent.click(backdrop);
+    
+            const modal = screen.queryByTestId('modal');
+            expect(modal).not.toBeInTheDocument();
+        })
+        it('prevents scrolling when a modal is open', () => {
+            openModal();
+    
+            const body = document.body;
+            expect(body).toHaveStyle('overflow: hidden');
+        })
+        it('restores page scrolling when the modal is closed', () => {
+            openModal();
+    
+            const closeButton = screen.getByTestId('modal-backdrop');
+            fireEvent.click(closeButton);
+    
+            const body = document.body;
+            expect(body).not.toHaveStyle('overflow: hidden');
+        })
     })
-    it('should render the modal component when set using hook', () => {
-        render(<CustomTest />)
-
-        const openButton = screen.getByText('Open Modal');
-        fireEvent.click(openButton);
-
-        const modal = screen.getByTestId('modal');
-        expect(modal).toBeInTheDocument();
-    })
-    it('should render a backdrop when the modal is open', () => {
-        render(<CustomTest />)
-
-        const openButton = screen.getByText('Open Modal');
-        fireEvent.click(openButton);
-
-        const backdrop = screen.getByTestId('modal-backdrop');
-        expect(backdrop).toBeInTheDocument();
-    })
-    it('should close the modal when click on backdrop', () => {
-        render(<CustomTest />)
-
-        const openButton = screen.getByText('Open Modal');
-        fireEvent.click(openButton);
-
-        const backdrop = screen.getByTestId('modal-backdrop');
-        fireEvent.click(backdrop);
-
-        const modal = screen.queryByTestId('modal');
-        expect(modal).not.toBeInTheDocument();
-    })
-    it('should close the modal when closeModal function is called', () => {
-        render(<CustomTest />)
-
-        const openButton = screen.getByText('Open Modal');
-        fireEvent.click(openButton);
-
-        const closeButton = screen.getByText('Close Modal');
-        fireEvent.click(closeButton);
-
-        const modal = screen.queryByTestId('modal');
-        expect(modal).not.toBeInTheDocument();
-    })
-    it('should prevent scrolling when modal is open', () => {
-        render(<CustomTest />)
-
-        const openButton = screen.getByText('Open Modal');
-        fireEvent.click(openButton);
-
-        const body = document.body;
-        expect(body).toHaveStyle('overflow: hidden');
-    })
-    it('should restore scrolling when modal is closed', () => {
-        render(<CustomTest />)
-
-        const openButton = screen.getByText('Open Modal');
-        fireEvent.click(openButton);
-
-        const closeButton = screen.getByTestId('modal-backdrop');
-        fireEvent.click(closeButton);
-
-        const body = document.body;
-        expect(body).not.toHaveStyle('overflow: hidden');
+    describe('Render without modal provider', () => {
+        it('should throw an error if used outside of the provider', () => {
+            jest.spyOn(console, 'error').mockImplementation(() => jest.fn());
+            expect(() => renderWithoutProviders(<CustomTest />)).toThrow('useModal must be used within a ModalProvider');
+            jest.restoreAllMocks();
+        })
     })
 })
